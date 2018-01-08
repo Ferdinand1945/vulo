99a05574cda4906056da25d26f8b70aeb6abe941
Fernando Lopez <fernandolopez@Fernandos-MacBook-Pro-3.local>
2018-01-08 22:39:45 +0100

    header

--- wp-content/themes/VULO/header.php
+++ wp-content/themes/VULO/header.php
@@ -10,7 +10,7 @@
 
 	<title><?php echo get_bloginfo( 'name' ); ?> - <?php echo get_bloginfo( 'description' ); ?></title>
 	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
-	<link href="<?php echo get_bloginfo( 'template_directory' );?>/blog.css" rel="stylesheet">
+	<link href="<?php echo get_bloginfo( 'template_directory' );?>/style.css" rel="stylesheet">
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
@@ -21,7 +21,7 @@
 
 <body>
 
-	<div class="blog-masthead">
+	<div class="header">
 	<div class="container">
 		<nav class="blog-nav">
 			<a class="blog-nav-item active" href="<?php echo get_bloginfo( 'wpurl' );?>">Home</a>
