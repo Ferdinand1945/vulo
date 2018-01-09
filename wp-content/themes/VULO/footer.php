b28cdce69f3a048a2c6bc152184b5c255cb9a222
Michael Vu <michaelvu@Michaels-MacBook-Pro.local>
2018-01-09 01:59:22 +0100

    skrivit till text på footern, ändrat färg

--- wp-content/themes/VULO/footer.php
+++ wp-content/themes/VULO/footer.php
@@ -1,10 +1,33 @@
   </div> <!-- /.container -->
 
-		<footer class="blog-footer">
-      <p>Blog template built for <a href="http://getbootstrap.com">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
-      <p>
-        <a href="#">Back to top</a>
-      </p>
+		<footer class="footer">
+    <div class="row">
+          <div class="col-md-4 col-lg-3">  
+            <p>
+        <strong>VU & LOPEZ HB</strong>
+                <br>
+                Harplingegränd 16, 125 73
+                <br>
+                Älvsjö, SWEDEN
+        </p>
+              </div>
+        <div class="col-md-4 col-lg-3">  
+            <p>
+        <strong>KONTAKTA</strong>
+                <br>
+                Telefonnummer: 070-071 23 45
+                <br>
+                Älvsjö, SWEDEN
+        </p>
+              </div>
+        <div class="col-md-4 col-lg-3">  
+            <p>
+        <strong>VULO</strong>
+              
+        </p>
+        </div>
+    
+            </div>
     </footer>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
