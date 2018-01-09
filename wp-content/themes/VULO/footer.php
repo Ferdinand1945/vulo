6dc060ecfb6dee76180f968cd313a857b6a68347
Michael Vu <michaelvu@Michaels-MacBook-Pro.local>
2018-01-09 22:43:38 +0100

    finslipning klar med footer

--- wp-content/themes/VULO/footer.php
+++ wp-content/themes/VULO/footer.php
@@ -1,47 +1,45 @@
-  </div> <!-- /.container -->
+</div> <!-- /.container -->
 
-		<footer class="footer">
-            <div class="container">
-    <div class="row">
-          <div class="col-md-4 col-lg-3">
-              <p>
-                <strong>VU & LOPEZ HB</strong>
-                <br>
-                Harplingegränd 16, 125 73
-                <br>
-                Älvsjö, SWEDEN
-              </p>
-    </div>
-        <div class="col-md-4 col-lg-3">  
-            <p>
-        <strong>KONTAKTA</strong>
-                <br>
-                Telefonnummer: 070-071 23 45
-                <br>
-                Mailadress: info@vulo.se
-        </p>
-              </div>
-       
-                </div>
-                <hr>
-                
-                    <div class="container">
-                        <div class="row">
-                    <div class="fb-like" data-href="https://www.facebook.com/vulo.se/" data-layout="button_count" data-action="like" data-size="small" data-show-faces="true" data-share="false"></div>
-                            
-                            <div class="right">© 2017 Vu & Lopez HB - Innehar F-skattsedel</div>
-                        
-                        </div>
-                
-                </div>
-        
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-lg-3">
+                <p>
+                    <strong>VU & LOPEZ HB</strong>
+                    <br>
+                    Harplingegränd 16, 125 73
+                    <br>
+                    Älvsjö, SWEDEN
+                </p>
             </div>
-            
-    </footer>
+            <div class="col-md-4 col-lg-3">  
+                <p>
+                    <strong>KONTAKTA</strong>
+                    <br>
+                    Telefonnummer: 070-071 23 45
+                    <br>
+                    Mailadress: info@vulo.se
+                </p>
+            </div>
 
+        </div>
+        <hr>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+        <div class="container">
+            <div class="row">
+                <div class="col noPaddingLeft">
+                    <div class="fb-like" data-href="https://www.facebook.com/vulo.se/" data-layout="button_count" data-action="like" data-size="small" data-show-faces="true" data-share="false">
+                    </div>
+                </div>
+                <div class="col noPaddingRight">
+                    <div class="right">© 2017 Vu & Lopez HB - Innehar F-skattsedel</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 <?php wp_footer(); ?> 
-  </body>
+</body>
 </html>
