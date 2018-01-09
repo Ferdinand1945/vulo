e934945b36389fc75b81e9e5d43b82e0a024e281
Fernando Lopez <fernandolopez@Fernandos-MacBook-Pro-3.local>
2018-01-09 22:59:03 +0100

    logo fixed

--- wp-content/themes/VULO/header.php
+++ wp-content/themes/VULO/header.php
@@ -55,8 +55,7 @@
 	<div class="header">
 	<div class="container">
 		<div class="logo">
-		<h1 class=""><a href="<?php echo get_bloginfo( 'wpurl' );?>"><img src=""></a></h1> <?php /*echo get_bloginfo( 'name' ); */?>
-			<p class=""><?php echo get_bloginfo( 'description' ); ?></p>
+		<h1 class=""><a href="<?php echo get_bloginfo( 'wpurl' );?>">VULO</h1> <?php /*echo get_bloginfo( 'name' ); */?>
 		</div>
 		<nav class="blog-nav">
 			<li><a class="blog-nav-item active" href="<?php echo get_bloginfo( 'wpurl' );?>">Home</a></li>
