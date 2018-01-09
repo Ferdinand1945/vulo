3b2129ae7f7fd66f359704f52db48e3140645de0
Fernando Lopez <fernandolopez@Fernandos-MacBook-Pro-3.local>
2018-01-09 14:55:03 +0100

    fix in layout index

--- wp-content/themes/VULO/index.php
+++ wp-content/themes/VULO/index.php
@@ -1,16 +1,16 @@
 <?php get_header(); ?>
 <?php include 'slider.php'; ?>
-<section>
+<section class="smaller-section">
 <div class="container">
    
 
    <div class="row">
    <div class="col">
       <h1>Vi bygger din hemsida</h1>
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur finibus nisi maximus, pharetra nulla finibus, efficitur mi. Phasellus at ligula quis nunc varius rhoncus. Sed vitae justo at nisl ornare fermentum at sit amet leo. Donec lobortis elementum faucibus. Vestibulum tempor nec justo in iaculis. Duis tellus mauris, c</p>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur finibus nisi maximus, pharetra nulla finibus, efficitur mi. Phasellus at ligula quis nunc varius rhoncus. Sed vitae justo at nisl ornare fermentum at sit amet leo. Donec lobortis elementum faucibus. Vestibulum tempor nec justo in iaculis. Duis tellus mauris, cLorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur finibus nisi maximus, pharetra nulla finibus, efficitur mi. Phasellus at ligula quis nunc varius rhoncus. Sed vitae justo at nisl ornare fermentum at sit amet leo. Donec lobortis elementum faucibus. Vestibulum tempor nec justo in iaculis. Duis tellus mauris, cLorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur finibus nisi maximus, pharetra nulla finibus, efficitur mi. Phasellus at ligula quis nunc varius rhoncus. Sed vitae justo at nisl ornare fermentum at sit amet leo. Donec lobortis elementum faucibus. Vestibulum tempor nec justo in iaculis. Duis tellus mauris, c</p>
     </div>
     <div class="col">
-      	<img src="http://localhost:8888/vulo/wp-content/uploads/2018/01/servers_01_new.png">
+      	<img style="width: 100%;" src="http://localhost:8888/vulo/wp-content/uploads/2018/01/servers_01_new.png">
     </div>
   
   </div>
